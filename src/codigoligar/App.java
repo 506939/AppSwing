f5903f4392c8fc1dd3ac124d06a3130d8dbfc23b
Daniel Matos Laranjeira <matosdaniel778@gmail.com>
2021-07-01 15:45:55 -0300

    Primeiro commit

---
+++ src/codigoligar/App.java
@@ -0,0 +1,33 @@
+package codigoligar;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class App {
+    private JPanel painelprincipal;
+    private JButton botão_msg;
+
+    public App() {
+
+        botão_msg.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+                JOptionPane.showMessageDialog(null, "A verdade está lá fora.");
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("App");
+        frame.setContentPane(new App() .painelprincipal);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.pack();
+        frame.setVisible(true);
+
+
+
+    }
+}
